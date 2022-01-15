1d6be8f404b1ff4a1edf25887884b690f23d53b1
Joedson Ferreira <joedson.devel@gmail.com>
2022-01-14 22:00:15 -0300

    API create
    construi uma api basica, usando alguns recursos como express, body,parser, cors e morgan

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const routes = require('./config/routes');
 
 // configurando o servidor
 
@@ -11,8 +12,9 @@ app.use(morgan('dev')); // log de execução
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.json()); // determinando o tipo de dados recebidos, em json
 app.use(cors());
+app.use(routes);
 
-app.listen(3002, () => {
+app.listen(3000, () => {
 
     console.log('Servidor rodando');
 });
